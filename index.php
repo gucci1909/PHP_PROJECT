039d3ece395a291339c45cdbdace33c044e0c693
gucci1909 <umangarora0134@gmail.com>
2023-01-24 21:45:20 +0530

    first php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP PROJECT</title>
 </head>
 <body>
     <h1>hello world</h1>
     <div>
 
     <?php 
-    echo "hello BABY"
+    Echo "hello BABY"
     ?>
     </div>
 </body>
